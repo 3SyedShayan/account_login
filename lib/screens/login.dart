d27b831836bfb9453881c18ba7184097753782c1
Syed Shayan <3syedshayan@gmail.com>
2025-07-23 20:49:14 +0500

    Firebase User Create added

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,53 +11,103 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLogin = false;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+  void onSubmit() async {
+    bool isValid = _form.currentState!.validate();
+    print('Form is valid: $isValid');
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+    if (_isLogin) {
+      print(
+        'Logging in with email: $_enteredEmail and password: $_enteredPassword',
+      );
+    } else {
+      try {
+        final userCredential = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+        print('User created: ${userCredential.user?.email}');
+      } on FirebaseAuthException catch (error) {
+        print('Error creating user: $error');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Form(
+          key: _form,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
 
-          children: [
-            Text(
-              'Login to your \naccount.',
-              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-            ),
-            Text("Please sign in to your account"),
-            SizedBox(height: 25),
-            Text(
-              "Email Address",
-              style: TextStyle(fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter Email',
-                border: OutlineInputBorder(),
+            children: [
+              Text(
+                'Login to your \naccount.',
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
               ),
-            ),
-            Text("Password", style: TextStyle(fontWeight: FontWeight.w500)),
-            SizedBox(height: 10),
+              Text("Please sign in to your account"),
+              SizedBox(height: 25),
+              Text(
+                "Email Address",
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Enter Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty || !value.contains('@')) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredEmail = value!;
+                },
+              ),
+              Text("Password", style: TextStyle(fontWeight: FontWeight.w500)),
+              SizedBox(height: 10),
 
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter Password',
-                border: OutlineInputBorder(),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: 'Enter Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+                onChanged: (value) {},
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 6) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _enteredPassword = value!;
+                },
               ),
-              obscureText: true,
-              onChanged: (value) {
-                // Handle password input changes
-              },
-            ),
-            Text("Forgot Password?"),
-            ElevatedButton(
-              onPressed: () {
-                // Handle login logic
-              },
-              child: Text('Login'),
-            ),
-          ],
+              Text("Forgot Password?"),
+              ElevatedButton(
+                onPressed: () {
+                  onSubmit();
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
