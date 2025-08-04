765a468af9eea95b89010268c8101cb33bf5b407
Syed Shayan <3syedshayan@gmail.com>
2025-08-04 15:07:31 +0500

    Dashboard UI improved

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:account_login/widgets/dashboard_header.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -8,80 +9,90 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  List<String> cities = ["Islamabad", "Karachi", "Lahore"];
-  String selectedCity = "Islamabad";
+  int selectedCategory = 0;
+  void onSelectCategory(int index) {
+    setState(() {
+      selectedCategory = index;
+    });
+  }
+
+  List<Map<String, dynamic>> categories = [
+    {"icon": Icons.fastfood, "label": "Burger"},
+    {"icon": Icons.cake, "label": "Desserts"},
+    {"icon": Icons.local_drink, "label": "Drinks"},
+    {"icon": Icons.local_pizza, "label": "Pizza"},
+  ];
+
+  Widget onCategorySelected() {
+    return Container(
+      height: 100,
+      child: ListView(
+        padding: EdgeInsets.only(left: 20),
+        scrollDirection: Axis.horizontal,
+        children: categories.map((category) {
+          int index = categories.indexOf(category);
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(0, 5, 10, 5),
+            child: GestureDetector(
+              onTap: () => onSelectCategory(index),
+              child: Container(
+                // padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: index == selectedCategory
+                      ? Colors.orange[400]
+                      : Colors.transparent,
+
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                width: 80,
+                // height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      categories[index]["icon"],
+                      size: 28,
+                      color: Colors.black,
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      index == selectedCategory
+                          ? categories[index]["label"]
+                          : "",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Stack(
-            children: [
-              Image.asset("assets/images/fast-food-cover.jpg"),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 20, 10, 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        DropdownButton(
-                          value: selectedCity,
-                          icon: Icon(
-                            Icons.arrow_drop_down,
-                            color: Colors.white,
-                          ),
-                          items: cities.map((String city) {
-                            return DropdownMenuItem(
-                              value: city,
-                              child: Text(city),
-                              onTap: () {},
-                            );
-                          }).toList(),
-                          selectedItemBuilder: (BuildContext context) {
-                            return cities.map((String city) {
-                              return Text(
-                                "Your Location",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                ),
-                              );
-                            }).toList();
-                          },
-                          onChanged: (value) {
-                            setState(() {
-                              selectedCity = value.toString();
-                            });
-                          },
-                        ),
-                        SizedBox(width: 20),
-                        Icon(Icons.search, color: Colors.white),
-                        SizedBox(width: 5),
-                        Icon(Icons.notifications, color: Colors.white),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Icon(Icons.location_on, color: Colors.white),
-                        Text(
-                          selectedCity,
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Provide The Best\nFood For you",
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                  ],
+          DashboardHeader(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 10, 10),
+            child: Row(
+              children: [
+                Text("Find by Category"),
+                Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "See All",
+                    style: TextStyle(color: Colors.yellow[800]),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          onCategorySelected(),
         ],
       ),
     );
---
+++ lib/widgets/dashboard_header.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class DashboardHeader extends StatefulWidget {
+  DashboardHeader({super.key});
+
+  @override
+  State<DashboardHeader> createState() => _DashboardHeaderState();
+}
+
+class _DashboardHeaderState extends State<DashboardHeader> {
+  List<String> cities = ["Islamabad", "Karachi", "Lahore"];
+
+  String selectedCity = "Islamabad";
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset("assets/images/fast-food-cover.jpg"),
+        Positioned(
+          top: 50,
+          right: 20,
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search, color: Colors.white, size: 35),
+          ),
+        ),
+        Positioned(
+          top: 50,
+          right: 65,
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_none_outlined,
+              color: Colors.white,
+              size: 35,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  DropdownButton(
+                    value: selectedCity,
+                    icon: Icon(Icons.arrow_drop_down, color: Colors.white),
+                    items: cities.map((String city) {
+                      return DropdownMenuItem(
+                        value: city,
+                        child: Text(city),
+                        onTap: () {
+                          setState(() {});
+                        },
+                      );
+                    }).toList(),
+                    selectedItemBuilder: (BuildContext context) {
+                      return cities.map((String city) {
+                        return Text(
+                          "Your Location",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        );
+                      }).toList();
+                    },
+                    onChanged: (value) {
+                      selectedCity = value.toString();
+                    },
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Icon(Icons.location_on, color: Colors.white),
+                  Text(
+                    selectedCity,
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Provide The Best\nFood For you",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
