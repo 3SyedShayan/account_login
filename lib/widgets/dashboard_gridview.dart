219f8fb4b7124fde756cc23ef5074f7902c009f1
Syed Shayan <3syedshayan@gmail.com>
2025-08-07 12:41:45 +0500

    Single Food Built separately

--- lib/widgets/dashboard_gridview.dart
+++ lib/widgets/dashboard_gridview.dart
@@ -1,5 +1,6 @@
 import 'package:account_login/models/dummy_data.dart';
 import 'package:account_login/screens/meal_detail.dart';
+import 'package:account_login/widgets/single_food.dart';
 import 'package:flutter/material.dart';
 
 class DashboardGridView extends StatelessWidget {
@@ -27,83 +28,7 @@ class DashboardGridView extends StatelessWidget {
       ),
       itemCount: foodCategories.length,
       itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => MealDetailScreen(
-                  mealCategory: foodCategories[index].category.name,
-                  mealIndex: index,
-                ),
-              ),
-            );
-          },
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    width: 160,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: AssetImage(foodCategories[index].imageUrl),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    right: 10,
-                    top: 10,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-
-                      child: Icon(Icons.favorite_border, color: Colors.red),
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                foodCategories[index].name,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.star, color: Colors.amber, size: 16),
-                  SizedBox(width: 5),
-                  Text(
-                    foodCategories[index].ratings.toString(),
-                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                  ),
-                  SizedBox(width: 10),
-                  Icon(Icons.timer, color: Colors.grey, size: 16),
-                  Text(
-                    foodCategories[index].duration.toString() + " min",
-                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.attach_money_outlined,
-                    color: Colors.green,
-                    size: 16,
-                  ),
-                  Text(
-                    foodCategories[index].price.toString(),
-                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
+        return SingleFood(foodCategories: foodCategories, index: index);
       },
     );
   }
