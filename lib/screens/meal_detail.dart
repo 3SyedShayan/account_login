47cb34208ad21bd49f277f6491d6e23750d1f985
Syed Shayan <3syedshayan@gmail.com>
2025-08-07 15:01:43 +0500

    reusable TextStyles added

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -1,5 +1,7 @@
 import 'package:account_login/models/dummy_data.dart';
 import 'package:account_login/models/food_item.dart';
+import 'package:account_login/utils/text_styles.dart';
+import 'package:account_login/widgets/single_food.dart';
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatefulWidget {
@@ -75,117 +77,144 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
           ),
         ],
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(3.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.asset(
-                      selectedFood.imageUrl,
-
-                      width: totalWidth,
-                      height: totalWidth,
-
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 15, 20, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
                   children: [
-                    Text(
-                      'Meal Detail',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 5),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.attach_money_outlined,
-                          color: Colors.yellow[800],
-                        ),
-                        Text(
-                          ' ${selectedFood.price.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.yellow[800],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.restaurant_menu, color: Colors.yellow[800]),
-                        Text('Free Delivery', style: TextStyle(fontSize: 18)),
-                        // SizedBox(width: 30),
-                        Spacer(),
-                        Icon(Icons.access_time, color: Colors.yellow[800]),
-                        Text(
-                          '${selectedFood.duration}',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        Spacer(),
-                        // SizedBox(width: 30),
-                        Icon(Icons.star, color: Colors.yellow[800]),
-                        Text(
-                          '${selectedFood.ratings}',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Here you can view the details of the selected meal.',
-                      style: TextStyle(fontSize: 16),
-                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        selectedFood.imageUrl,
 
-                    Divider(color: Colors.grey, thickness: 1, height: 30),
-                    Text(
-                      "Description",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                        width: totalWidth,
+                        height: totalWidth,
+
+                        fit: BoxFit.cover,
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      selectedFood.description,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text("Recommended For You"),
-                        Spacer(),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "See All",
-                            style: TextStyle(color: Colors.yellow[800]),
-                          ),
-                        ),
-                      ],
                     ),
                   ],
                 ),
-              ),
-            ],
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 15, 20, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        selectedFood.name,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.attach_money_outlined,
+                            color: Colors.yellow[800],
+                          ),
+                          Text(
+                            ' ${selectedFood.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.yellow[800],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      buildRow(selectedFood),
+                      Divider(color: Colors.grey, thickness: 1, height: 30),
+                      Text(
+                        "Description",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        selectedFood.description,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text("Recommended For You"),
+                          Spacer(),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "See All",
+                              style: TextStyle(color: Colors.yellow[800]),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: totalWidth * 0.4,
+                            height: totalWidth * 0.4,
+                            child: SingleFood(
+                              foodCategories: foodData
+                                  .where(
+                                    (item) =>
+                                        item.category.name.toLowerCase() ==
+                                        widget.mealCategory.toLowerCase(),
+                                  )
+                                  .toList(),
+                              index: 3,
+                            ),
+                          ),
+                          Container(
+                            width: totalWidth * 0.4,
+                            height: totalWidth * 0.4,
+                            child: SingleFood(
+                              foodCategories: foodData
+                                  .where(
+                                    (item) =>
+                                        item.category.name.toLowerCase() ==
+                                        widget.mealCategory.toLowerCase(),
+                                  )
+                                  .toList(),
+                              index: 1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildRow(FoodItem selectedFood) {
+    return Row(
+      children: [
+        Icon(Icons.restaurant_menu, color: Colors.yellow[800]),
+        SizedBox(width: 5),
+        Text('${selectedFood.name}', style: AppTextStyles.bodySmall),
+        Spacer(),
+        Icon(Icons.access_time, color: Colors.yellow[800]),
+        Text('${selectedFood.duration}', style: AppTextStyles.bodySmall),
+        Spacer(),
+        Icon(Icons.star, color: Colors.yellow[800]),
+        Text('${selectedFood.ratings}', style: AppTextStyles.bodySmall),
+      ],
     );
   }
 }
---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles(this.text);
+  String text;
+  static const TextStyle bodySmall = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+  );
+}
