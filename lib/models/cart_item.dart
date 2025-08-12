1c3936d3b41701de2c267e79cce25ee29a780b1c
Syed Shayan <3SyedShayan@gmail.com>
2025-08-12 12:07:13 +0500

    Created Cart Model

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,8 @@
+import 'package:account_login/models/food_item.dart';
+
+class CartItem {
+  final FoodItem foodItem;
+  final int quantity;
+
+  CartItem({required this.foodItem, required this.quantity});
+}
